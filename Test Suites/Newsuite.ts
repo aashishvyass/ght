6299e578bbfef4f89e71a9f394c94bbd155019a6
Ashish <aashishvyass@gmail.com>
2019-05-15 17:59:06 +0530

    first commit

---
+++ Test Suites/Newsuite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Newsuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>cc447fa6-6ec7-40ab-85e8-5c7e7d629684</testSuiteGuid>
+   <testCaseLink>
+      <guid>36d697c8-f401-4505-85e8-77501801db4e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/NavigateURL</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
